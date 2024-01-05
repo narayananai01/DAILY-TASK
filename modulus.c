3a8597fb2058879de2d2aee82a74fe3f92a0ea76
narayananaio1 <naveen28ai01@gmail.com>
2024-01-05 10:35:29 +0530

    added modulus

---
+++ modulus.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a;
+    int b=2;
+    int c;
+    printf("enter the value of A:");
+    scanf("%d",&a);
+    c=a%b;
+    if (c==0)
+    
+    {
+        printf("given valus is even");
+    }
+    else
+    {
+        printf("given value is odd");
+    }
+    return 0;
+}
